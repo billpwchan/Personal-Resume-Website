cbb13df108e0cd3f9d0dc1bddfba4f9a1ba958ea
Bill Chan <billpwchan@hotmail.com>
2019-01-03 19:25:32 +0000

    Awesome Font Completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './da-slide/css/style.css']
 })
 export class AppComponent {
-  title = 'BillResumeWebsite';
+  title = 'Bill\'s Resume';
+  introHeader = 'I develop website, softwares, and learning Machine Learning.\n Yes, I love Technology.';
+  introContent = 'Experienced Quality Assurance and Software Engineer \
+  with a demonstrated history of working in the Information Technology Industry. \
+  Skilled in Java, Python, Full-Stack Web Development with Angular, MySQL, etc. Currently pursuing \
+  a Dual Degree Program (BEng in Computer Science & BBA Business Management) from the Hong Kong University \
+  of Science and Technology.';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +14,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
