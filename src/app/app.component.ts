d94dfbcdd2c51c83fe379f4a7859b9c409b02113
Bill Chan <billpwchan@hotmail.com>
2019-01-06 00:16:33 +0000

    Before Adding Router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { PageScrollConfig } from 'ngx-page-scroll';
 import { SwiperModule, SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { TextAnimation } from 'ngx-teximate';
+import { fadeInDown } from 'ng-animate';
+import { GalleryItem, ImageItem } from '@ngx-gallery/core';
+import { SwiperDirective } from 'ngx-swiper-wrapper';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css', './da-slide/css/style.css', './fontawesome/css/all.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  text = 'Lorem ipsum dolor sit amet.';
+
+  enterAnimation: TextAnimation = {
+    animation: fadeInDown,
+    delay: 50,
+    type: 'letter'
+  };
+
+
   public slides = [
     {
-      'year': '2016',
-      'title': 'Title',
-      'text': 'Text'
+      'photo': 'url(https://unsplash.it/1920/500?image=11)',
+      'year': 'Apr 2017 - Present',
+      'title': 'Quality Assurance Engineer \n-- MeteorSpark Inc.',
+      'text': '• Performed manual/automation Quality Assurance on JustDo.today (Manual & Automated via Puppeteer + Chromium)\n• Examined compatibility on multiple platforms, including Android, IOS and PC'
+    },
+    {
+      'photo': 'url(https://unsplash.it/1920/500?image=11)',
+      'year': 'Jun 2018 - Nov 2018',
+      'title': 'IVVQ T&C Software Engineer Intern \n-- Thales HK',
+      'text': '• Enhanced Test Automated Tool for conducting DDQS workflow testing on the main control system of Ground Transportation (Front-End: JavaFX, Back-End: Java, Hibernate & SQLite)\n• Develop Real - time Trend for monitoring station equipment\' data (Front-End: Angular 5 + Bootstrap; Back-End: STOMP WebSocket Interface, retrieving data from SCADASoft Connector)'
+    },
+    {
+      'photo': 'url(https://unsplash.it/1920/500?image=11)',
+      'year': 'Jan 2018 - Feb 2018',
+      'title': 'Wealth Management Intern \n-- AIA',
+      'text': '• Participated in International Fund House seminars on private wealth management\n• Gained in-depth understanding of daily operations within a listed company, for future career development'
+    },
+    {
+      'photo': 'url(https://unsplash.it/1920/500?image=11)',
+      'year': 'Jun 2017 - Aug 2017',
+      'title': 'Construction Project Manager Intern \n-- CLP HK',
+      'text': '• Performed R&D on Construction Permit Monitoring Dashboard project, delivered a fully-functional product after peer evaluation and testing.\n• Implemented automation utilities for data handling systems, including contractor monthly high-risk activities reporting system, KPI reporting system, etc.'
+    },
+    {
+      'photo': 'url(https://unsplash.it/1920/500?image=11)',
+      'year': 'Jan 2017 - Apr 2017',
+      'title': 'Junior Web Developer \n-- Sanuker Group Limited',
+      'text': '• Full-stack web development using ReactJS, Redux, and Meteor.\n• Developed web-version database visualization utility for MongoDB.\n• Utilized Bootstrap and Material-UI for improving the appearance of the deliverable web solution product.'
     }
   ];
 
   public config: SwiperConfigInterface = {
     direction: 'vertical',
-    slidesPerView: 1,
+    slidesPerView: 'auto',
     observer: true,
-    keyboard: true,
+    keyboard: false,
     mousewheel: false,
     scrollbar: false,
     navigation: true,
-    pagination: false,
+    pagination: true,
     loop: false,
     speed: 1600,
   };
+
+  @ViewChild('swiper-container') swiperView: SwiperDirective;
+
 
   title = 'Bill\'s Resume';
   introHeader = 'I develop website, software, and studying Machine Learning.\n Yes, I love Technology.';
@@ -65,5 +107,12 @@ export class AppComponent {
         return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
       }
     };
+  }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.swiperView.startAutoplay();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GalleryModule, GALLERY_CONFIG } from '@ngx-gallery/core';
+import { LightboxModule } from '@ngx-gallery/lightbox';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { NgxPageScrollModule } from 'ngx-page-scroll';
 import { SwiperConfigInterface, SwiperModule, SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { TeximateModule } from 'ngx-teximate';
 import { AppComponent } from './app.component';
 
 
@@ -22,17 +26,28 @@ const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
     AngularFontAwesomeModule,
     NgxPageScrollModule,
-    SwiperModule
+    SwiperModule,
+    TeximateModule,
+    GalleryModule,
+    LightboxModule
   ],
   providers: [
     {
       provide: SWIPER_CONFIG,
       useValue: DEFAULT_SWIPER_CONFIG
+    },
+    {
+      provide: GALLERY_CONFIG,
+      useValue: {
+        dots: true,
+        imageSize: 'cover'
+      }
     }
   ],
   bootstrap: [AppComponent]
